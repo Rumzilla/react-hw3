d67798a8b038a2de4d4755bb8fd7d10239886878
Rumzilla <wow7am@gmail.com>
2023-01-10 01:42:21 +0600

    add materialui

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import {Link, Route, Switch } from "react-router-dom"
 import HomePage from './pages/home'
 import AboutPage from './pages/about'
 import SignPage from './pages/sign'
+import { AppBar, Button, Toolbar, Typography } from "@mui/material"
+import { Box, Container } from "@mui/system"
 
 const App = () => {
   return (
     <div>
       <header>
-        <div>
-          <p><Link to='/pages/home'>Home</Link></p>
-          <p><Link to='/pages/about'>About</Link></p>
-          <p><Link to='/pages/sign'>Sign In</Link></p>
-        </div>
+    <AppBar position="fixed">
+      <Container fixed>
+        <Toolbar>
+          <Typography variant="h5">React Homework</Typography>
+          <Box mr={0} padding={5}>
+            <Button color='inherit' variant="outlined">
+              <Link to='/pages/home'>Home</Link>
+            </Button>
+            <Button color='inherit' variant="contained">
+              <Link to='/pages/about'>About</Link>
+            </Button>
+            <Button color='inherit' variant="outlined">
+              <Link to='/pages/sign'>Sign In</Link>
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
       </header>
 
-      <hr />
+    <hr />
 
-      <main>
+    <main>
         <Switch>
           <Route exact path='/pages/home'>
             <HomePage />
@@ -35,3 +50,29 @@ const App = () => {
 }
 
 export default App
+
+// <div>
+    //   <header>
+    //     <div>
+    //       <p><Link to='/pages/home'>Home</Link></p>
+    //       <p><Link to='/pages/about'>About</Link></p>
+    //       <p><Link to='/pages/sign'>Sign In</Link></p>
+    //     </div>
+    //   </header>
+
+    //   <hr />
+
+    //   <main>
+    //     <Switch>
+    //       <Route exact path='/pages/home'>
+    //         <HomePage />
+    //       </Route>
+    //       <Route exact path='/pages/about'>
+    //         <AboutPage />
+    //       </Route>
+    //       <Route exact path='/pages/sign'>
+    //         <SignPage />
+    //       </Route>
+    //     </Switch>
+    //   </main>
+    // </div>
